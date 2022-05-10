6c52a701f50689c4e1d11c6764ab3b0e75ba2ceb
kagan <kagancosar@gmail.com>
2022-05-10 16:14:40 +0300

    Jwt eklendi ve configu rasyonları yapıldı

--- MovieAPI.API/Startup.cs
+++ MovieAPI.API/Startup.cs
@@ -1,9 +1,13 @@
+using System.Text;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MovieAPI.DataTier.Concrete;
 using MovieAPI.DataTier.Context;
@@ -12,6 +16,7 @@ using MovieAPI.ServiceTier.AutoMapper.Profiles;
 using MovieAPI.ServiceTier.Concrete;
 using MovieAPI.ServiceTier.Filters;
 using MovieAPI.ServiceTier.Interfaces;
+using MovieAPI.ServiceTier.JWT;
 using MovieAPI.ServiceTier.Validators.Managers;
 using MovieAPI.ServiceTier.Validators.Movies;
 
@@ -29,12 +34,41 @@ namespace MovieAPI.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            #region Jwt
+
+            var appsettingSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appsettingSection);
+
+            var appsettings=appsettingSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appsettings.SecurityKey);
+            services.AddAuthentication(a =>
+            {
+                a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                a.DefaultChallengeScheme= JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(b =>
+            {
+                b.RequireHttpsMetadata = false;//http
+                b.SaveToken = true;
+                b.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),//imza key i symetric asimetric ile olsun
+                    ValidateIssuer = false,
+                    ValidateAudience = true,
+                };
+            });
+
+            #endregion
+
             services.AddScoped<IMovieRepository, MovieRepository>();
             services.AddScoped<IMovieService, MovieService>();
             services.AddScoped<IManagerRepository, ManagerRepository>();
             services.AddScoped<IManagerService, ManagerService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserRepository, UserRepository>();
 
             services.AddScoped<IActorService, ActorService>();
             services.AddScoped<IActorRepository, ActorRepository>();
@@ -63,6 +97,8 @@ namespace MovieAPI.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
---
+++ MovieAPI.ServiceTier/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieAPI.ServiceTier.Dtos.User;
+using MovieAPI.ServiceTier.JWT;
+
+namespace MovieAPI.ServiceTier.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<AccessToken> Authanticate(UserLoginDto userLogin);
+    }
+}
