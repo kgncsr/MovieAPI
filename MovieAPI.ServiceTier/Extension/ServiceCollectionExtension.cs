8d7098000f214845875dc8f729b196873be24e0c
kagan <kagancosar@gmail.com>
2022-05-20 15:47:06 +0300

    IServiceCollection icin extension metot olusturulması.

---
+++ MovieAPI.ServiceTier/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using MovieAPI.DataTier.Concrete;
+using MovieAPI.DataTier.Interfaces;
+using MovieAPI.ServiceTier.Concrete;
+using MovieAPI.ServiceTier.Interfaces;
+using MovieAPI.ServiceTier.JWT.Concrete;
+using MovieAPI.ServiceTier.JWT.Interface;
+
+namespace MovieAPI.ServiceTier.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection MyService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IMovieRepository, MovieRepository>();
+            serviceCollection.AddScoped<IMovieService, MovieService>();
+            serviceCollection.AddScoped<IManagerRepository, ManagerRepository>();
+            serviceCollection.AddScoped<IManagerService, ManagerService>();
+            serviceCollection.AddScoped<ICategoryService, CategoryService>();
+            serviceCollection.AddScoped<ICategoryRepository, CategoryRepository>();
+            serviceCollection.AddScoped<IUserService, UserService>();
+            serviceCollection.AddScoped<IUserRepository, UserRepository>();
+            serviceCollection.AddScoped<IActorService, ActorService>();
+            serviceCollection.AddScoped<IActorRepository, ActorRepository>();
+            serviceCollection.AddScoped<ITokenService, TokenService>();
+
+
+
+            return serviceCollection;
+        }
+
+
+    }
+}
